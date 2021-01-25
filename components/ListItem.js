bd6e9f15ddf6ade4eaa68278bab441e5c4af008e
j4zuu <jasu.honkanen2@gmail.com>
2021-01-25 14:55:51 +0200

    comp lib done

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,75 +1,30 @@
-/* eslint-disable linebreak-style */
 import React from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
-import { uploadsUrl } from '../utils/variables';
-
-/*
- __
-(oO)\_________
-(__)\         )\/\
-     ||----w||
-     ||     ||
-*/
+import {uploadsUrl} from '../utils/variables';
+import {Avatar, ListItem as RNEListItem} from 'react-native-elements';
 
 const ListItem = ({navigation, singleMedia}) => {
+  // console.log(props);
   return (
-    <TouchableOpacity
-      style={styles.row}
+    <RNEListItem
+      bottomDivider
       onPress={() => {
         navigation.navigate('Single', {file: singleMedia});
       }}
     >
-      <View style={styles.imagebox}>
-        <Image
-          style={styles.image}
-          source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
-        />
-      </View>
-      <View>
-        <Text style={styles.listTitle}>{singleMedia.title}</Text>
-        <Text style={styles.textbox}>{singleMedia.description}</Text>
-      </View>
-    </TouchableOpacity>
+      <Avatar
+        size="large"
+        square
+        source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
+      ></Avatar>
+      <RNEListItem.Content>
+        <RNEListItem.Title h4>{singleMedia.title}</RNEListItem.Title>
+        <RNEListItem.Subtitle>{singleMedia.description}</RNEListItem.Subtitle>
+      </RNEListItem.Content>
+      <RNEListItem.Chevron />
+    </RNEListItem>
   );
 };
-
-const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#636363',
-    padding: 15,
-    marginBottom: 5,
-    borderRadius: 16,
-    marginHorizontal: 5,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-
-    elevation: 3,
-  },
-  image: {
-    flex: 1,
-    borderRadius: 6,
-    width: 100,
-  },
-  imagebox: {
-    marginRight: 5,
-  },
-  textbox: {
-    width: 250,
-    color: 'white',
-  },
-  listTitle: {
-    fontWeight: 'bold',
-    color: 'white',
-    fontSize: 20,
-  },
-});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
